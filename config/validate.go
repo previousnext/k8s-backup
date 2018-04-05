101588c08074bc7af6379208196a86f1869adffb
Nick Schuch <nick@myschuch.com>
2018-04-05 12:13:55 +1000

    Initial code commit

---
+++ config/validate.go
@@ -0,0 +1,46 @@
+package config
+
+import (
+	"github.com/pkg/errors"
+)
+
+// Validate the config.
+func (c Config) Validate() error {
+	if c.Namespace == "" {
+		return errors.New("not found: namespace")
+	}
+
+	if c.Image == "" {
+		return errors.New("not found: image")
+	}
+
+	if c.Prefix == "" {
+		return errors.New("not found: prefix")
+	}
+
+	if c.Bucket == "" {
+		return errors.New("not found: bucket")
+	}
+
+	if c.Frequency == "" {
+		return errors.New("not found: frequency")
+	}
+
+	if c.Resources.CPU == "" {
+		return errors.New("not found: cpu")
+	}
+
+	if c.Resources.Memory == "" {
+		return errors.New("not found: resources: memory")
+	}
+
+	if c.Credentials.ID == "" {
+		return errors.New("not found: credentials: id")
+	}
+
+	if c.Credentials.Secret == "" {
+		return errors.New("not found: credentials: secret")
+	}
+
+	return nil
+}
